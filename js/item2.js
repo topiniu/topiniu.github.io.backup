819dd24393b5f9470dc67940c9cff421cc638b5b
topiniu <topiniu@outlook.com>
2017-03-07 13:51:55 +0800

    第三屏

--- js/item2.js
+++ js/item2.js
@@ -8,32 +8,32 @@
            "margin-left":"-" + marginLeft + "px"
        },500);
 
-       initFontSize(1);
-       var year = $(this).val();
-       clz = "year" + year;
-       initFontSize(0);
+       // initFontSize(1);
+       // var year = $(this).val();
+       // clz = "year" + year;
+       // initFontSize(0);
    });
 
-   function initFontSize(flag){
-       if(flag) {
-           $('.' + clz + " .my_year p").css({
-               "font-size": "65px"
-           });
-           $('.' + clz + " .my_title p").css({
-               "font-size": "30px"
-           });
-           $('.' + clz + " .my_content p").css({
-               "font-size": "22px"
-           });
-       }else{
-           $('.' + clz + " .my_year p").css({
-               "font-size":"71px"
-           });
-           $('.' + clz + " .my_title p").css({
-               "font-size":"35px"
-           });
-           $('.' + clz + " .my_content p").css({
-               "font-size":"25px"
-           });
-       }
-   }
+   // function initFontSize(flag){
+   //     if(flag) {
+   //         $('.' + clz + " .my_year p").css({
+   //             "font-size": "65px"
+   //         });
+   //         $('.' + clz + " .my_title p").css({
+   //             "font-size": "30px"
+   //         });
+   //         $('.' + clz + " .my_content p").css({
+   //             "font-size": "22px"
+   //         });
+   //     }else{
+   //         $('.' + clz + " .my_year p").css({
+   //             "font-size":"71px"
+   //         });
+   //         $('.' + clz + " .my_title p").css({
+   //             "font-size":"35px"
+   //         });
+   //         $('.' + clz + " .my_content p").css({
+   //             "font-size":"25px"
+   //         });
+   //     }
+   // }
