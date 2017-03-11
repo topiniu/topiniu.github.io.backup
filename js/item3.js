623f3cbb277b0f3d43e316360bae824b7c7596fe
topiniu <topiniu@outlook.com>
2017-03-11 18:31:34 +0800

    样式动画修改
    为3屏作品item增加立体倾斜效果
    
    更改2屏时间轴的样式

--- js/item3.js
+++ js/item3.js
@@ -11,4 +11,43 @@ $(function () {
            "background-color":"rgba(255,255,255,0.3)"
        });
    }) ;
+
+
+    $(".item3 .project_preview").mouseenter(function(){
+        // alert(0);
+        // $(this + " a").css({
+        //         "background-color":"rgba(255,255,255,0)"
+        // });
+
+        var thisPX = $(this).offset().left;
+        var thisPY = $(this).offset().top;
+
+        var boxWidth = $(this).outerWidth();
+        var boxHeight = $(this).outerHeight();
+
+        $(this).addClass("threeD");
+
+        $(".threeD").mousemove(function(e){
+            var mouseX = e.pageX - thisPX;
+            var mouseY = e.pageY - thisPY;
+
+            var X = mouseX - boxWidth/2;
+            var Y = boxHeight/2 - mouseY;
+
+            $(".threeD").css({
+                "transform":"rotateY("+X/10+"deg) rotateX("+Y/10+"deg)"
+            });
+        });
+    });
+
+    $(".item3 .project_preview").mouseleave(function(){
+        $(this).removeClass("threeD");
+        $(this).css({
+            "transform": "rotateY(0deg) rotateX(0deg)"
+        });
+
+        // $(this + " a").css({
+        //     "background-color":"rgba(255,255,255,0.3)"
+        // });
+    })
 });
