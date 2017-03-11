916fce186ba5fab9c14a7380a127c254b95f9ad3
topiniu <topiniu@outlook.com>
2017-03-11 21:53:45 +0800

    细节优化

--- js/item2.js
+++ js/item2.js
@@ -9,17 +9,17 @@
            "transform":"scale(1)"
        });
 
-       var marginLeft = $(this).data("item");
-       $(".item2 .itemContainer").animate({
-           "margin-left":"-" + marginLeft + "px"
-       },500);
-
        var year = $(this).data("val");
        clz = year;
        $(".year" + clz).css({
            "opacity":"1",
            "transform":"scale(1.4)"
        });
+
+       var marginLeft = $(this).data("item");
+       $(".item2 .itemContainer").animate({
+           "margin-left":"-" + marginLeft + "px"
+       },200);
 
        // initFontSize(1);
        // var year = $(this).val();
@@ -41,9 +41,16 @@
                }
            });
 
+           setTimeout(function () {
+               $(".item2 .itemContainer").css({
+                   "left": "40%",
+                   "transform": "scale(1)",
+                   "opacity": "1"
+               });
+           }, 500);
        }else{
            $(".item2 .itemContainer").css({
-               "left": "70%",
+               "left": "40.5%",
                "transform": "scale(0.7)",
                "opacity": "0.3"
            });
