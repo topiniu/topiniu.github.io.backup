5cac22c2d78a280d82c397bae743cc056d35d415
topiniu <topiniu@outlook.com>
2017-04-10 06:44:09 +0800

    更换主页图片

--- js/item1.js
+++ js/item1.js
@@ -8,7 +8,7 @@ function loadImg(){
     var box = $("#imgBox");
 
     // alert(img.attr("src"));
-    var url = "http://topiniu.com/img/testImg.png";
+    var url = "http://topiniu.com/img/main.jpg";
     img.attr("src",url);
     img.addClass("myImg");
     img.css("z-index","1");
