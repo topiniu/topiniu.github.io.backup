3c6b3d5ee662ea2323c2d0199375bd307f3cad0b
萌康 <mengkang@showjoy.com>
2017-06-09 16:27:37 +0800

    add color

--- js/item3.js
+++ js/item3.js
@@ -2,31 +2,49 @@
  * Created by topiniu on 2017/3/7.
  */
 $(function () {
-
+    randomColor();
     $(".project_preview").hover(function () {
         $(this).find('.cover').addClass('show');
         }, function () {
-            $(this).find('.cover').removeClass('show');
+        $(this).find('.cover').removeClass('show');
     });
+
 
 });
 
 function showProject(flag){
-    if(flag==1) {
-        $(".item3 .project_preview").css({
-            "transform": "scale(1.1) rotateY(0deg) rotateX(0deg)",
-            "transition": " all .3s"
-        });
+    if(flag==1) {//show
+      $(".item3 .project_preview").each(function(index,item){
+	      setTimeout(function () {
+		      $(item).animate({
+			      left:  '0px'
+		      },600);
+	      },index*100);
+      });
 
-        setTimeout(function () {
-            $(".item3 .project_preview").css({
-                "transform": "scale(1) rotateY(0deg) rotateX(0deg)"
-            });
-        }, 300)
-    }else{
-        $(".item3 .project_preview").css({
-            "transform": "scale(0) rotateY(0deg) rotateX(0deg)",
-            "transition": " all .1s"
-        });
+    }else{//hide
+      var scrollLeft = $('.item3 .window').scrollLeft();
+      var total = scrollLeft+1000;
+        $(".item3 .project_preview").animate({
+            left: total + 'px'
+        },100);
     }
+}
+
+function randomColor(){
+    var num = 0;
+    var flag = 0;
+    var classname = '';
+
+    $('.project_preview').each(function(index,item){
+        flag = parseInt(Math.random()*8,10) + 1;
+        while(flag === num){
+	        flag = parseInt(Math.random()*8,10) + 1;
+        };
+
+        num = flag;
+        classname = 'panel_style' + num;
+
+        $(item).addClass(classname);
+    });
 }
